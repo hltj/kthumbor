c4515757cb1db7e265be1a0ca4eb63ce233d1c41
hltj <jiaywe@gmail.com>
2018-11-08 22:21:18 +0800

    [parser] implement thumbnail parameter parsing (width & height)

--- parser/src/main/kotlin/ThumbnailParameter.kt
+++ parser/src/main/kotlin/ThumbnailParameter.kt
@@ -5,4 +5,13 @@ import me.hltj.kthumbor.share.ThumbnailParameter
 /**
  * parse a string ([this]) into [ThumbnailParameter] or `null` if failed
  */
-fun String.toThumbnailParameter(): ThumbnailParameter? = TODO("not implemented")
+fun String.toThumbnailParameter(): ThumbnailParameter? {
+    val groups = paramRegex.matchEntire(this)?.groups ?: return null
+
+    val width = groups["width"]?.value?.toIntOrNull() ?: 0
+    val height = groups["height"]?.value?.toIntOrNull() ?: 0
+
+    return if (width == 0 && height == 0) null else ThumbnailParameter(width, height)
+}
+
+private val paramRegex = Regex("""(?<width>[1-9](?:[0-9]*))?(?:x(?<height>[1-9](?:[0-9]*)))?""")
