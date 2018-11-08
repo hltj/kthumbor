7aacdecd2838119d6533c2b7448dee6356d30d2f
hltj <jiaywe@gmail.com>
2018-11-08 08:42:27 +0800

    [parser] implement `String.toThumbnailUrlPath()` with unimplemented `String.splitFromEnd()` & add test cases for `String.splitFromEnd()`

--- parser/src/main/kotlin/ThumbnailUrlPath.kt
+++ parser/src/main/kotlin/ThumbnailUrlPath.kt
@@ -11,4 +11,13 @@ data class ThumbnailUrlPath(
  * e.g.:
  * `"/path/to/image.suffix0.param.suffix"` to `ThumbnailUrlPath("/path/to/image.suffix0", "param", "suffix")`
  */
-fun String.toThumbnailUrlPath(): ThumbnailUrlPath? = TODO("not implemented")
+fun String.toThumbnailUrlPath(): ThumbnailUrlPath? {
+    val list = splitFromEnd('.', 2)
+    return if (list.size != 3 || list.any { it.isEmpty() }) {
+        null
+    } else {
+        ThumbnailUrlPath(list[0], list[1], list[2])
+    }
+}
+
+internal fun String.splitFromEnd(delimiter: Char, limit: Int = length): List<String> = TODO("not implemented")
--- parser/src/test/kotlin/ThumbnailUrlPathTest.kt
+++ parser/src/test/kotlin/ThumbnailUrlPathTest.kt
@@ -3,6 +3,7 @@ package me.hltj.kthumbor.parser.test
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import me.hltj.kthumbor.parser.ThumbnailUrlPath
+import me.hltj.kthumbor.parser.splitFromEnd
 import me.hltj.kthumbor.parser.toThumbnailUrlPath
 
 class ThumbnailUrlPathTest : StringSpec({
@@ -40,5 +41,89 @@ class ThumbnailUrlPathTest : StringSpec({
 
     "no origin path => null" {
         ".param.suffix".toThumbnailUrlPath() shouldBe null
+    }
+})
+
+class SplitFromEnd : StringSpec({
+    "a-b => [a, b]" {
+        "a-b".splitFromEnd('-') shouldBe listOf("a", "b")
+    }
+
+    "a- => [a, ]" {
+        "a-".splitFromEnd('-') shouldBe listOf("a", "")
+    }
+
+    "-b => [, b]" {
+        "-b".splitFromEnd('-') shouldBe listOf("", "b")
+    }
+
+    "a-b-c, ? => [a, b, c]" {
+        val s = "a-b-c"
+        val list = listOf("a", "b", "c")
+        s.splitFromEnd('-') shouldBe list
+        s.splitFromEnd('-', 10) shouldBe list
+        s.splitFromEnd('-', 3) shouldBe list
+        s.splitFromEnd('-', 2) shouldBe list
+    }
+
+    "a-bc, ? => [a, bc]" {
+        val s = "a-bc"
+        val list = listOf("a", "bc")
+        s.splitFromEnd('-') shouldBe list
+        s.splitFromEnd('-', 10) shouldBe list
+        s.splitFromEnd('-', 3) shouldBe list
+        s.splitFromEnd('-', 2) shouldBe list
+        s.splitFromEnd('-', 1) shouldBe list
+    }
+
+    "a-b-c, 1 => [a-b, c]" {
+        "a-b-c".splitFromEnd('-', 1) shouldBe listOf("a-b", "c")
+    }
+
+    "abc, ? => [abc]" {
+        val s = "abc"
+        val list = listOf(s)
+        s.splitFromEnd('-') shouldBe list
+        s.splitFromEnd('-', 10) shouldBe list
+        s.splitFromEnd('-', 2) shouldBe list
+        s.splitFromEnd('-', 1) shouldBe list
+    }
+
+    "123-4-56789, ? => [123, 4, 56789]" {
+        val s = "123-4-56789"
+        val list = listOf("123", "4", "56789")
+        s.splitFromEnd('-') shouldBe list
+        s.splitFromEnd('-', 10) shouldBe list
+        s.splitFromEnd('-', 2) shouldBe list
+    }
+
+    "123-4-56789, 1 => [123-4, 56789]" {
+        "123-4-56789".splitFromEnd('-', 1) shouldBe listOf("123-4", "56789")
+    }
+
+    "----, ? => [, , , ,]" {
+        val list = listOf("", "", "", "", "")
+        "----".splitFromEnd('-') shouldBe list
+        "----".splitFromEnd('-', 10) shouldBe list
+        "----".splitFromEnd('-', 5) shouldBe list
+        "----".splitFromEnd('-', 4) shouldBe list
+    }
+
+    "----, 3 => [-, , , ]" {
+        "----".splitFromEnd('-', 3) shouldBe listOf("-", "", "", "")
+    }
+
+    "----, 2 => [--, , ]" {
+        "----".splitFromEnd('-', 2) shouldBe listOf("--", "", "")
+    }
+
+    "----, 1 => [---, ]" {
+        "----".splitFromEnd('-', 1) shouldBe listOf("---", "")
+    }
+
+    "----, ? => [----]" {
+        "----".splitFromEnd('-', 0) shouldBe listOf("----")
+        "----".splitFromEnd('-', -1) shouldBe listOf("----")
+        "----".splitFromEnd('-', -9) shouldBe listOf("----")
     }
 })
