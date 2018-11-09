113d0c541c1b5a575abf0c315a031b56aa268aa2
hltj <jiaywe@gmail.com>
2018-11-09 08:50:58 +0800

    [generator] implement the thumbnail generating function

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 dependencies {
     implementation(project(":share"))
     implementation(kotlin("stdlib-jdk8"))
+    implementation(group = "net.coobird", name = "thumbnailator", version = "0.4.8")
     testImplementation(kotlin("reflect"))
     testImplementation(group = "io.kotlintest", name = "kotlintest-runner-junit5", version = "3.1.10")
     testImplementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.3")
--- generator/src/main/kotlin/ThumbnailGenerator.kt
+++ generator/src/main/kotlin/ThumbnailGenerator.kt
@@ -2,9 +2,16 @@ package me.hltj.kthumbor.generator
 
 import me.hltj.kthumbor.share.ThumbnailParameter
 
+import net.coobird.thumbnailator.Thumbnails
 import java.awt.image.BufferedImage
+import kotlin.math.roundToInt
 
 /**
  * generate a thumbnail with [parameter] for a [BufferedImage]
  */
-operator fun BufferedImage.times(parameter: ThumbnailParameter): BufferedImage = TODO("not implemented")
+operator fun BufferedImage.times(parameter: ThumbnailParameter): BufferedImage {
+    val w = if (parameter.width > 0) parameter.width else (parameter.height * width / height.toDouble()).roundToInt()
+    val h = if (parameter.height > 0) parameter.height else (parameter.width * height / width.toDouble()).roundToInt()
+
+    return if (w > width && h > height) this else Thumbnails.of(this).size(w, h).asBufferedImage()
+}
