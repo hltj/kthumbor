29c568775521d485401591b2e034a2db29594eeb
hltj <jiaywe@gmail.com>
2018-11-10 21:43:45 +0800

    implement for / & favicon

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,9 +1,20 @@
 package me.hltj.kthumbor
 
 import io.ktor.application.*
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
 fun Application.module() {
-    TODO("not implemented")
+    routing {
+        get("/") {
+            call.respondText("Kthumbor - a thumbnail service")
+        }
+
+        static("/") {
+            resources("static")
+        }
+    }
 }
