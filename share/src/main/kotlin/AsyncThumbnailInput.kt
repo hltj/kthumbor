042c7d3aaa66de55b44a1b57d8164adcb3244131
hltj <jiaywe@gmail.com>
2018-11-13 08:05:45 +0800

    [share] add a data class AsyncThumbnailInput

---
+++ share/src/main/kotlin/AsyncThumbnailInput.kt
@@ -0,0 +1,9 @@
+package me.hltj.kthumbor.share
+
+import java.awt.image.BufferedImage
+
+data class AsyncThumbnailInput(
+    val image: BufferedImage,
+    val parameter: ThumbnailParameter,
+    val format: String
+)
