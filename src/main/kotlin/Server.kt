6478f7bf13162694758fee392f90b8bc9c8d9a05
hltj <jiaywe@gmail.com>
2018-11-16 22:52:30 +0800

    enable CallId & CallLogging features

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,13 +1,27 @@
 package me.hltj.kthumbor
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import java.util.concurrent.atomic.AtomicLong
 
 fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
 fun Application.module() {
+    install(CallId) {
+        header(HttpHeaders.XRequestId)
+
+        val counter = AtomicLong()
+        generate { "hltj-me-${counter.incrementAndGet()}" }
+    }
+
+    install(CallLogging) {
+        callIdMdc("request-id")
+    }
+
     routing {
         get("/") {
             call.respondText("Kthumbor - a thumbnail service")
