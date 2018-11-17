738e69ec8838b0c69c11ee024a071345351f569b
hltj <jiaywe@gmail.com>
2018-11-17 12:47:59 +0800

    add test cases for basic thumbnails

--- src/test/kotlin/ServerTest.kt
+++ src/test/kotlin/ServerTest.kt
@@ -30,6 +30,66 @@ class ServerTest : StringSpec({
             }
         }
     }
+
+    "200 /oss.png.80x80.png" {
+        withTestApplication(Application::module) {
+            with(handleRequest(HttpMethod.Get, "/oss.png.80x80.png").response) {
+                status() shouldBe HttpStatusCode.OK
+                byteContent shouldBe staticResourceOf("/oss.png.80x80.png").readBytes()
+            }
+        }
+    }
+
+    "200 /oss.png.60.jpg" {
+        withTestApplication(Application::module) {
+            with(handleRequest(HttpMethod.Get, "/oss.png.60.jpg").response) {
+                status() shouldBe  HttpStatusCode.OK
+                byteContent shouldBe staticResourceOf("/oss.png.60x60.jpg").readBytes()
+            }
+        }
+    }
+
+    "200 /oss.png.x40.gif" {
+        withTestApplication(Application::module) {
+            with(handleRequest(HttpMethod.Get, "/oss.png.x40.gif").response) {
+                status() shouldBe HttpStatusCode.OK
+                byteContent shouldBe staticResourceOf("/oss.png.40x40.gif").readBytes()
+            }
+        }
+    }
+
+    "200 /oss.png.30x20.bmp" {
+        withTestApplication(Application::module) {
+            with(handleRequest(HttpMethod.Get, "/oss.png.30x20.bmp").response) {
+                status() shouldBe HttpStatusCode.OK
+                byteContent shouldBe staticResourceOf("/oss.png.20x20.bmp").readBytes()
+            }
+        }
+    }
+
+    "400 /oss.png.not-param.png" {
+        withTestApplication(Application::module) {
+            with(handleRequest(HttpMethod.Get, "/oss.png.notparam.png").response) {
+                status() shouldBe HttpStatusCode.BadRequest
+            }
+        }
+    }
+
+    "400 /oss.png.80x30.txt" {
+        withTestApplication(Application::module) {
+            with(handleRequest(HttpMethod.Get, "/oss.png.80x30.txt").response) {
+                status() shouldBe HttpStatusCode.BadRequest
+            }
+        }
+    }
+
+    "404 /no-such-image.80x30.png" {
+        withTestApplication(Application::module) {
+            with(handleRequest(HttpMethod.Get, "/no-such-image.80x30.png").response) {
+                status() shouldBe HttpStatusCode.NotFound
+            }
+        }
+    }
 })
 
 internal fun TestContext.staticResourceOf(relativePath: String) =
