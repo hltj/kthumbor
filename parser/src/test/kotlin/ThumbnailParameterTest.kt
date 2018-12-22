f7dedc39e49cf19c479f120a6177eab9ebea17e2
hltj <jiaywe@gmail.com>
2018-12-22 13:39:56 +0800

    [parser] add test cases for unimplemented parsing enlargeable parameter

--- parser/src/test/kotlin/ThumbnailParameterTest.kt
+++ parser/src/test/kotlin/ThumbnailParameterTest.kt
@@ -4,7 +4,7 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import me.hltj.kthumbor.parser.toThumbnailParameter
 
-class ThumbnailParameterTest : StringSpec({
+class ThumbnailParameterBasicTest : StringSpec({
     "1677x9 => w1677 h9" {
         with("1677x9".toThumbnailParameter()!!) {
             width shouldBe 1677
@@ -63,5 +63,84 @@ class ThumbnailParameterTest : StringSpec({
 
     "123-235 => null" {
         "123-235".toThumbnailParameter() shouldBe null
+    }
+})
+
+class ThumbnailParameterEnlargeableTest: StringSpec({
+    "40x30 => w40 h30 non-enlargeable" {
+        with("40x30".toThumbnailParameter()!!) {
+            width shouldBe 40
+            height shouldBe 30
+            enlargeable shouldBe false
+        }
+    }
+
+    "40x30e => w40 h30 enlargeable" {
+        with("40x30e".toThumbnailParameter()!!) {
+            width shouldBe 40
+            height shouldBe 30
+            enlargeable shouldBe true
+        }
+    }
+
+    "40 => w40 non-enlargeable" {
+        with("40".toThumbnailParameter()!!) {
+            width shouldBe 40
+            height shouldBe 0
+            enlargeable shouldBe false
+        }
+    }
+
+    "40e => w40enlargeable" {
+        with("40e".toThumbnailParameter()!!) {
+            width shouldBe 40
+            height shouldBe 0
+            enlargeable shouldBe true
+        }
+    }
+
+    "x30 => h30 non-enlargeable" {
+        with("x30".toThumbnailParameter()!!) {
+            width shouldBe 0
+            height shouldBe 30
+            enlargeable shouldBe false
+        }
+    }
+
+    "x30e => h30 enlargeable" {
+        with("x30e".toThumbnailParameter()!!) {
+            width shouldBe 0
+            height shouldBe 30
+            enlargeable shouldBe true
+        }
+    }
+
+
+    "40x030e => null" {
+        "40x030e".toThumbnailParameter() shouldBe null
+    }
+
+    "040x30e => null" {
+        "040x30e".toThumbnailParameter() shouldBe null
+    }
+
+    "40x0e => null" {
+        "40x0e".toThumbnailParameter() shouldBe null
+    }
+
+    "0x30e => null" {
+        "0x30e".toThumbnailParameter() shouldBe null
+    }
+
+    "0x0e => null" {
+        "0x0e".toThumbnailParameter() shouldBe null
+    }
+
+    "xe => null" {
+        "xe".toThumbnailParameter() shouldBe null
+    }
+
+    "40x30E => null" {
+        "40x30E".toThumbnailParameter() shouldBe null
     }
 })
