5917d809a2566dad788a889a55616493792bcf89
hltj <jiaywe@gmail.com>
2019-09-03 13:05:55 +0800

    read image with Dispatchers.IO context

--- src/main/kotlin/HttpImageFetcher.kt
+++ src/main/kotlin/HttpImageFetcher.kt
@@ -8,15 +8,18 @@ import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import io.ktor.client.features.*
 import io.ktor.util.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @UseExperimental(KtorExperimentalAPI::class)
 private val httpClient = HttpClient(CIO)
 
 suspend fun httpImageOf(path: String): KthumborResult<BufferedImage> = try {
     val bytes = httpClient.get<ByteArray>("http://localhost:8080$path")
-
-    @Suppress("BlockingMethodInNonBlockingContext")
-    KthumborResult.Success(ImageIO.read(bytes.inputStream()).withAlpha())
+    val image = withContext(Dispatchers.IO) {
+        ImageIO.read(bytes.inputStream())
+    }
+    KthumborResult.Success(image.withAlpha())
 } catch (e: ResponseException) {
     if (e.response.status == HttpStatusCode.NotFound) KthumborResult.NotFound else KthumborResult.BadInput
 } catch (e: Exception) {
